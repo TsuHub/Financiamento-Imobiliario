830ae5798e17c56d814f1c39ca37d163c7274b82
TsuHub <tsuy_1988@hotmail.com>
2021-12-18 17:38:05 -0300

    Criado o endpoint de preenchimento

--- V0/FinancingRequest/src/main/java/com/financingrequest/FinancingRequestApplication.java
+++ V0/FinancingRequest/src/main/java/com/financingrequest/FinancingRequestApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinancingRequestApplication {
-
+public class FinancingRequestApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(FinancingRequestApplication.class, args);
 	}
-
 }
---
+++ V0/FinancingRequest/src/main/java/com/financingrequest/models/FinancingRequest.java
@@ -0,0 +1,41 @@
+package com.financingrequest.models;
+
+public class FinancingRequest
+{
+    private String requestDate;         // Data da solicitação
+    private String requesterName;       // Nome do solicitante
+    private double financingValue;      // Valor do financiamento
+    private double income;              // Rendimentos
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getRequesterName() {
+        return requesterName;
+    }
+
+    public void setRequesterName(String requesterName) {
+        this.requesterName = requesterName;
+    }
+
+    public double getFinancingValue() {
+        return financingValue;
+    }
+
+    public void setFinancingValue(double financingValue) {
+        this.financingValue = financingValue;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+}
