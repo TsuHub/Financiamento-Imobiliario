39e5e40abeb4209dbafd2dd6ca2e6f99dab911c6
Putzeys <gputzeys@gmail.com>
2021-12-19 00:37:34 -0300

    ain

---
+++ V0/ProcessServer/src/main/java/org/usp/each/gpn/workflow/messages/broker_to_bank/SendSolicitation.java
@@ -0,0 +1,15 @@
+package org.usp.each.gpn.workflow.messages.broker_to_bank;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SendSolicitation implements JavaDelegate
+{
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        delegateExecution.getProcessEngineServices().getRuntimeService()
+                .createMessageCorrelation("TratamentoBanco")
+                .correlate();
+    }
+}
