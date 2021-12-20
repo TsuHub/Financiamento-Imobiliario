fb3f42c2941ac8eea0ee17fad61b29640daebf68
TsuHub <tsuy_1988@hotmail.com>
2021-12-20 00:04:57 -0300

    Modelo funcionando corretamente

--- V0/ProcessServer/src/main/java/com/gpn/workflow/messages/client/to_bank/SubmissionProposalChoicedToBank.java
+++ V0/ProcessServer/src/main/java/com/gpn/workflow/messages/client/to_bank/SubmissionProposalChoicedToBank.java
@@ -9,7 +9,7 @@ public class SubmissionProposalChoicedToBank implements JavaDelegate
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
         delegateExecution.getProcessEngineServices().getRuntimeService()
-                .createMessageCorrelation("ClientePropostaEscolhidaBanco")
+                .createMessageCorrelation("PropostaEscolhidaBanco")
                 .correlate();
     }
 }
